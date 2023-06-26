d9733558caa7e806426d2bffec5150a1f4466f97
sherlyhu28 <sherlyhu28@gmail.com>
2023-06-26 20:06:57 +0700

    yeyyy the data can be displayed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,19 @@
-export default function Home(){
-    return <h2>Hello</h2>
+import {db} from "../lib/db";
+import { doc, getDoc } from "firebase/firestore";
+
+export let getStaticProps = async () => {
+    let docRef = doc(db, "identity", "identity");
+    let getData = await getDoc(docRef);
+    let data = JSON.parse(JSON.stringify(getData.data()));
+    return {props: {data}}
+}
+
+export default function Home({data}){
+    return (
+        <>
+            <h2>{data.name}</h2>
+            <p>{data.age}</p>
+            <p>{data.hobby}</p>
+        </>
+    )
 }
