2ea23fe467fc6639ee9bfaf41ad3246fc4fb6eed
sherlyhu28 <sherlyhu28@gmail.com>
2023-06-27 16:04:52 +0700

    testing production mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
 import {db} from "../lib/db";
+import Layout from "../lib/layout.js";
 import { doc, getDoc } from "firebase/firestore";
 
-export let getStaticProps = async () => {
+export async function getStaticProps(){
     let docRef = doc(db, "identity", "identity");
     let getData = await getDoc(docRef);
     let data = JSON.parse(JSON.stringify(getData.data()));
     return {props: {data}}
 }
 
-export default function Home({data}){
+export default function Home({data}:any){
     return (
-        <>
+        <Layout>
             <h2>{data.name}</h2>
             <p>{data.age}</p>
             <p>{data.hobby}</p>
-        </>
+        </Layout>
     )
 }
